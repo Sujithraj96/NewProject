fa93d628416548fb50cb8e91cedc9607bda61a23
Sujithraj96 <rssujithraj96@gmail.com>
2021-09-23 12:51:46 +0530

    statement added

---
+++ src/org/projectGit/Sample.java
@@ -0,0 +1,12 @@
+package org.projectGit;
+
+public class Sample {
+	
+	
+	private void tc1() {
+		System.out.println("browser");
+		System.out.println("username");
+		System.out.println("password");
+
+	}
+}
